ca94b76bc548533d5a1faca088ce6e0642d6ff82
Doonguin <penguinicesp@gmail.com>
2023-01-09 16:41:00 +0100

    Eigen content toegevoegd

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+    msg: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <nav>
+        <a href="#">
+            <img alt="account" src="/public/account.png" />
+        </a>
+        <a href="#">
+            <img alt="account" src="/public/add.png" />
+        </a>
+        <a href="#">
+            <img alt="account" src="/public/remove.png" />
+        </a>
+    </nav>
+</template>
