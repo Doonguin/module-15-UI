4ffd9e27f57f4584f50165329fe2bfc72705866e
Doonguin <penguinicesp@gmail.com>
2023-01-09 16:41:18 +0100

    Eigen content tegevoegd

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -11,12 +11,15 @@ defineProps({
     <nav>
         <a href="#">
             <img alt="account" src="/public/account.png" />
+            ACCOUNT
         </a>
         <a href="#">
             <img alt="account" src="/public/add.png" />
+            ADD TO-DO
         </a>
         <a href="#">
             <img alt="account" src="/public/remove.png" />
+            REMOVE TO-DO
         </a>
     </nav>
 </template>
